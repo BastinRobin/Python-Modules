19c2485212f266db9ae754100c3a694e9929ba81
Bastin Robin <bastinrobins@gmail.com>
2013-12-21 22:54:12 +0530

    Added new module

---
+++ pyunderscore.py
@@ -0,0 +1,19 @@
+'''
+Author : Bastin Robin
+Email : bastinrobins@gmail.com
+Desc: A Simple Python Module For Data Manipulation
+'''
+
+robin = [{'name':'Robin', 'age':32},{'name':'Robin', 'age':32},{'name':'Robin', 'age':32}]
+
+class Py_score:
+	'''
+	main class starts
+	'''
+	def __init__ (self):
+		self.version = '0.0.1v'
+		self.author = 'Bastin Robin'
+
+
+if __name__ == '__main__':
+	main()
